ffbb18508b0240ca48123e27cfcb045ba804c65d
oddy-bassey <oddybassey@gmail.com>
2023-03-20 17:13:18 +0100

    sepearted env variables property source

--- src/main/java/com/fcmb/SeerbitAPI/service/SeerbitAPIServiceImpl.java
+++ src/main/java/com/fcmb/SeerbitAPI/service/SeerbitAPIServiceImpl.java
@@ -5,6 +5,7 @@ import com.fcmb.SeerbitAPI.dto.AuthRequest;
 import com.fcmb.SeerbitAPI.dto.BaseDtoEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +14,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 @RequiredArgsConstructor
+@PropertySource("classpath:env.properties")
 @Service
 public class SeerbitAPIServiceImpl implements SeerbitAPIService {
 
