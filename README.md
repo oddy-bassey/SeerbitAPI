059e211560c6d75cd7af2a805e89161aec785535
oddy-bassey <oddybassey@gmail.com>
2023-03-20 05:05:21 +0100

    initial project setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# SEERBIT Remittance Payout API
---
+++ src/main/java/com/fcmb/SeerbitAPI/SeerbitApiApplication.java
@@ -0,0 +1,13 @@
+package com.fcmb.SeerbitAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeerbitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeerbitApiApplication.class, args);
+	}
+
+}
